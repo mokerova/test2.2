07aa3d243280cef8f95350da5d414c336d2a00bf
mokerova <45970745+mokerova@users.noreply.github.com>
2019-06-29 14:50:50 +0300

    Update tests.h

--- tests/tests.h
+++ tests/tests.h
@@ -10,42 +10,33 @@ extern "C" {
 }
 
 using namespace testing;
-TEST(deleteLines, editortests)
-{
-    text txt = create_text();
-    load(txt, "inputLONG.txt");
-    delete_line(txt, 2 );
 
-    EXPECT_EQ(txt->length, 2);
-    delete_line(txt, 1);
-    EXPECT_EQ(txt->length, 1);
-}
 
-    // Объект структуры создался?
-TEST(test1, tests)
+TEST(test0, tests)
 {
     EXPECT_NE(create_text(), nullptr);
 }
-    // Загрузился несуществующий файл?
-TEST(test2, tests)
+
+
+TEST(test1, tests)
 {
     text txt = create_text();
     load(txt, "<filename>");
-
     EXPECT_EQ(txt->length, 0);
 }
-    // Нет прав на файл?
-TEST(test3, tests)
+
+
+TEST(test2, tests)
 {
     text txt = create_text();
     load(txt, "/etc/shadow");
 
     EXPECT_EQ(txt->length, 0);
 }
-    // Обычный тескт
 
-    // Текст вывелся в консоль?
-TEST(test4, tests)
+
+
+TEST(test3, tests)
 {
     text txt = create_text();
     show(txt);
@@ -54,80 +45,74 @@ TEST(test4, tests)
     EXPECT_EQ(txt->cursor->position, 0);
 }
 
-// Текст сохранился?
-TEST(test5, tests)
+
+TEST(test4, tests)
 {
     text txt = create_text();
     const char* contents = "TEST";
     load(txt, "input.txt");
-
     EXPECT_NO_FATAL_FAILURE(append_line(txt, contents););
     EXPECT_NE(txt->length, 0);
 }
-// К тексту возможно добавить строку?
-TEST(test7, tests)
+
+
+TEST(test5, tests)
 {
 text txt = create_text();
 const char* contents = "TEST";
 load(txt, "input.txt");
-
 EXPECT_NO_FATAL_FAILURE(append_line(txt, contents););
 EXPECT_NE(txt->length, 0);
 }
 
-// К тексту возможно добавить слишком длинную строку?
-TEST(test8, tests)
+TEST(test6, tests)
 {
 text txt = create_text();
 char contents[10000];
 load(txt, "input.txt");
 for (; strlen(contents) < 10000; strcat(contents, "TEST"));
-
 EXPECT_EXIT(append_line(txt, contents), ::testing::ExitedWithCode(EXIT_FAILURE), "");
 }
 
 
-// Возможно удалить все строки из текста?
-TEST(test11, tests)
+TEST(test7, tests)
 {
 text txt = create_text();
 load(txt, "input.txt");
 remove_all(txt);
-
 EXPECT_NE(txt, nullptr);
 EXPECT_EQ(txt->length, 0);
 }
-// Возможно удалить все строки из пустого текста?
-TEST(test12, tests)
+
+
+TEST(test8, tests)
 {
 text txt = nullptr;
 remove_all(txt);
-
 EXPECT_EQ(txt, nullptr);
 }
-// Возможно переместить курсор на данную позицию?
-TEST(test13, tests)
-{
-text txt = create_text();
-load(txt, "input.txt");
-m(txt, 1, 2);
 
-EXPECT_EQ(txt->cursor->position, 1);//5
-}
-// Возможно переместить курсор на позицию с отрицательными координатами?
-TEST(test14, tests)
+
+TEST(test9, tests)
 {
 text txt = create_text();
 load(txt, "input.txt");
 m(txt, 1, 2);
-m(txt, -1, -1);
 
 EXPECT_EQ(txt->cursor->position, 1);
 }
 
 
+TEST(test10, tests)
+{
+text txt = create_text();
+load(txt, "input.txt");
+m(txt, 1, 2);
+m(txt, -1, -1);
+EXPECT_EQ(txt->cursor->position, 1);
+}
 
-TEST(test15, tests)
+TEST(test11, tests)
 {
 text txt = create_text();
 load(txt, "inputLONG.txt");
@@ -136,7 +121,7 @@ showrew(txt);
 EXPECT_EQ(txt->cursor->position, 1);
 }
 
-TEST(test17, tests)
+TEST(test12, tests)
 {
 text txt = create_text();
 load(txt, "inputLONG.txt");
@@ -145,7 +130,7 @@ rt(txt);
 EXPECT_EQ(txt->cursor->position, 2);
 }
 
-TEST(test200, tests)
+TEST(test13, tests)
 {
 text txt = create_text();
 load(txt, "inputLONG.txt");
@@ -153,33 +138,40 @@ txt->cursor->position = 1;
 show(txt);
 EXPECT_EQ(txt->cursor->position, 1);
 }
-TEST(saveError, editortests)
+TEST(test14, tests)
 {
     text txt = create_text();
     load(txt, "input.txt");
-
     EXPECT_NE(txt->length, 0);
     save(txt, "test.txt");
     EXPECT_NE(fopen("test.txt", "r"), nullptr);
 }
 
-// Перемещение курсора в конец слова
-TEST(test18, tests)
+
+TEST(test15, tests)
 {
 text txt = create_text();
 load(txt, "input1.txt");
 m(txt, 1, 2);
 mwef(txt);
-
 EXPECT_EQ(txt->cursor->position, 4);
 }
 
-TEST(test19, tests)
+TEST(test16, tests)
 {
 text txt = create_text();
 load(txt, "input1.txt");
 m(txt, 1, 6);
 mwef(txt);
-
 EXPECT_EQ(txt->cursor->position, 9);
+}
+
+TEST(test17, tests)
+{
+    text txt = create_text();
+    load(txt, "inputLONG.txt");
+    delete_line(txt, 2 );
+    EXPECT_EQ(txt->length, 2);
+    delete_line(txt, 1);
+    EXPECT_EQ(txt->length, 1);
 }
